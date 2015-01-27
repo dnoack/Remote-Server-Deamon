d1aa9d06a87f621106ebb316fd3800067269c26b
dnoack <dnoack@work>
2015-01-27 15:40:36 +0100

    Adding autogenerated makefiles and eclipse config file
    Former-commit-id: 9a3e87827c4054e0bfd03b19cfc3fdd8ec2be9c0

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lzmq -lCppUTest -lCppUTestExt -laardvark -ldl -lpthread
+
