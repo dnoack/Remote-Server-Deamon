0dfaefe939802c2e671db090d2de06ae37d1f5fe
Dave <Dave@linux-1sjv.fritz.box>
2015-02-26 11:18:14 +0100

    Implemented the design with 2 interfaces as base class for worker like in Plugin-Server.
    Former-commit-id: 5e35e0fe0d011f1f74f8a7ecbad6bc1c3a5bef44

---
+++ include/TcpWorker.hpp
@@ -0,0 +1,81 @@
+/*
+ * TCPWorker.hpp
+ *
+ *  Created on: 09.02.2015
+ *      Author: dnoack
+ */
+
+#ifndef INCLUDE_TCPWORKER_HPP_
+#define INCLUDE_TCPWORKER_HPP_
+
+//unix domain socket definition
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstdio>
+#include <list>
+
+
+#include "JsonRPC.hpp"
+#include <pthread.h>
+#include "WorkerInterface.hpp"
+#include <WorkerThreads.hpp>
+#include "signal.h"
+
+
+class UdsClient;
+
+
+#define BUFFER_SIZE 1024
+#define ADD_WORKER true
+#define DELETE_WORKER false
+#define WORKER_FREE 0
+#define WORKER_BUSY 1
+#define WORKER_GETSTATUS 2
+
+
+class TcpWorker : public WorkerInterface, WorkerThreads{
+
+	public:
+		TcpWorker(int socket);
+		~TcpWorker();
+
+		int tcp_send(string* data);
+
+
+	private:
+
+
+		//variables for listener
+		bool listen_thread_active;
+		char receiveBuffer[BUFFER_SIZE];
+		int recvSize;
+
+
+		//variables for worker
+		bool worker_thread_active;
+		char* bufferOut;
+		string* jsonInput;
+		string* identity;
+		string* jsonReturn;
+
+
+		//not shared, more common
+		pthread_t lthread;
+		JsonRPC* json;
+		UdsClient* udsClient;
+		int currentSocket;
+
+
+		virtual void thread_listen(pthread_t partent_th, int socket, char* workerBuffer);
+
+		virtual void thread_work(int socket);
+
+
+};
+
+
+
+#endif /* INCLUDE_UDSWORKER_HPP_ */
---
+++ include/UdsWorker.hpp
@@ -0,0 +1,77 @@
+/*
+ * UdsWorker.hpp
+ *
+ *  Created on: 09.02.2015
+ *      Author: dnoack
+ */
+
+#ifndef INCLUDE_UDSWORKER_HPP_
+#define INCLUDE_UDSWORKER_HPP_
+
+//unix domain socket definition
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstdio>
+#include <list>
+
+
+#include "JsonRPC.hpp"
+#include <pthread.h>
+#include "signal.h"
+
+#include "TcpWorker.hpp"
+#include "WorkerInterface.hpp"
+#include "WorkerThreads.hpp"
+
+#define BUFFER_SIZE 1024
+#define ADD_WORKER true
+#define DELETE_WORKER false
+#define WORKER_FREE 0
+#define WORKER_BUSY 1
+#define WORKER_GETSTATUS 2
+
+
+class UdsWorker : public WorkerInterface, WorkerThreads{
+
+	public:
+		UdsWorker(int socket, TcpWorker* tcpWorker);
+		~UdsWorker();
+
+
+	private:
+
+
+		//variables for listener
+		bool listen_thread_active;
+		char receiveBuffer[BUFFER_SIZE];
+		int recvSize;
+
+
+		//variables for worker
+		bool worker_thread_active;
+		char* bufferOut;
+		string* jsonInput;
+		string* identity;
+		string* jsonReturn;
+
+
+
+		//not shared, more common
+		pthread_t lthread;
+		int currentSocket;
+		TcpWorker* tcpWorker;
+
+
+		virtual void thread_listen(pthread_t partent_th, int socket, char* workerBuffer);
+
+		virtual void thread_work(int socket);
+
+
+};
+
+
+
+#endif /* INCLUDE_UDSWORKER_HPP_ */
