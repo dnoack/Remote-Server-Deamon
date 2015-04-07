1158683057e2e9b1d66ce1c3b9c1ace613ce4582
dnoack <dnoack@work>
2015-04-07 14:04:27 +0200

    Test build setup !
    Former-commit-id: 059f24772a4ab8530fc5318e4381cfa107efca2d

---
+++ Testing-RSD/src/subdir.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/ConnectionContext.cpp \
+../src/JsonRPC.cpp \
+../src/RSD.cpp \
+../src/RsdMsg.cpp \
+../src/TcpWorker.cpp \
+../src/UdsComClient.cpp \
+../src/UdsComWorker.cpp \
+../src/UdsRegServer.cpp \
+../src/UdsRegWorker.cpp 
+
+OBJS += \
+./src/ConnectionContext.o \
+./src/JsonRPC.o \
+./src/RSD.o \
+./src/RsdMsg.o \
+./src/TcpWorker.o \
+./src/UdsComClient.o \
+./src/UdsComWorker.o \
+./src/UdsRegServer.o \
+./src/UdsRegWorker.o 
+
+CPP_DEPS += \
+./src/ConnectionContext.d \
+./src/JsonRPC.d \
+./src/RSD.d \
+./src/RsdMsg.d \
+./src/TcpWorker.d \
+./src/UdsComClient.d \
+./src/UdsComWorker.d \
+./src/UdsRegServer.d \
+./src/UdsRegWorker.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DTESTMODE -I/home/dnoack/libs/rapidjson/include/rapidjson -I"/home/dnoack/workspace/RSD-and-Plugin-lib/include" -O3 -Wall -c -fmessage-length=0 ${CXXFLAGS} -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
