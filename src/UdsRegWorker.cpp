e22e7390f0ff7c140b174c6b30e6059b407f7d1e
Dave <Dave@linux-1sjv.fritz.box>
2015-05-29 17:28:31 +0200

    RegWorker now works with tagged messages.

--- src/UdsRegWorker.cpp
+++ src/UdsRegWorker.cpp
@@ -81,65 +81,117 @@ void UdsRegWorker::thread_work()
 }
 
 
+
 void UdsRegWorker::thread_listen()
 {
-
 	listen_thread_active = true;
 	string* content = NULL;
-	int retval;
-	fd_set rfds;
-
+	int retval = 0;
 	pthread_t worker_thread = getWorker();
+
 
 	FD_ZERO(&rfds);
 	FD_SET(currentSocket, &rfds);
 
+
 	while(listen_thread_active)
 	{
-		retval = select(currentSocket+1, &rfds, NULL, NULL, NULL);
-
-		if(retval < 0)
-		{
-			//Something is wrong with our socket, maybe plugin closed the connection
-			listen_thread_active = false;
-			deletable = true;
-		}
-		else if(FD_ISSET(currentSocket, &rfds))
-		{
-			recvSize = recv(currentSocket , receiveBuffer, BUFFER_SIZE, 0);
-			//data received
-			if(recvSize > 0)
-			{
-				//add received data in buffer to queue
-				content = new string(receiveBuffer, recvSize);
-				pushReceiveQueue(new RsdMsg(0, content));
-				//signal the worker
-				pthread_kill(worker_thread, SIGUSR1);
-			}
-			else
-			{
-				listen_thread_active = false;
-				deletable = true;
-			}
-		}
-
 		memset(receiveBuffer, '\0', BUFFER_SIZE);
+
+		//hier nur hin wenn nichts mehr im buffer ist und auf keine teile einer nachricht gewartet wird.
+		recvSize = recv(currentSocket , receiveBuffer, BUFFER_SIZE, 0);
+		if(recvSize > 0)
+		{
+			//copy receiveBuffer to a clean msgBuffer
+			msgBufferSize = recvSize;
+			msgBuffer = new char[msgBufferSize];
+			memset(msgBuffer, '\0', msgBufferSize);
+			memcpy(msgBuffer, receiveBuffer, msgBufferSize);
+
+			//As long as there is data in the msgBuffer
+			while(msgBufferSize > 0)
+			{
+				//headersize = 1 byte tagfield + 4 byte lengthfield
+				if(msgBufferSize > HEADER_SIZE)
+				{
+					messageSize = readHeader(msgBuffer);
+
+					//header ok ???
+					if(messageSize > -1)
+					{
+						//Is there at least one complete message in msgBuffer ?
+						if(msgBufferSize >= messageSize+HEADER_SIZE)
+						{
+							//add first complete msg of msgbuffer to the receivequeue and signal the worker
+							content = new string(&msgBuffer[HEADER_SIZE], messageSize);
+							pushReceiveQueue(new RsdMsg(0, content));
+							pthread_kill(worker_thread, SIGUSR1);
+
+							//Is there more data ?
+							if(msgBufferSize > messageSize+HEADER_SIZE)
+							{
+								//copy rest of data to a new clean msgBuffer
+								msgBufferSize = msgBufferSize - (messageSize+HEADER_SIZE);
+								tempBuffer = new char[msgBufferSize];
+								memset(tempBuffer, '\0', msgBufferSize);
+								memcpy(tempBuffer, &(msgBuffer[messageSize+HEADER_SIZE]), msgBufferSize);
+								delete[] msgBuffer;
+								msgBuffer = tempBuffer;
+							}
+							else
+							{
+								delete[] msgBuffer;
+								msgBufferSize = 0;
+							}
+						}
+						//message is not complete, wait for the rest
+						else
+						{
+							waitForFurtherData();
+						}
+					}
+					else
+					{
+						delete[] msgBuffer;
+						msgBufferSize = 0;
+						//TODO: send msg back, that something went wrong and the message was not correctly received
+					}
+				}
+				//not even the header was complete, wait for the rest
+				else
+				{
+					waitForFurtherData();
+				}
+			}
+		}
+		else
+		{
+			deletable = true;
+			listen_thread_active = false;
+		}
 	}
 }
 
 
-
 int UdsRegWorker::transmit(const char* data, int size)
 {
+	int sendCount = 0;
+	createHeader(headerOut, size);
 	log(logInfoOut, data);
-	return send(currentSocket, data, size, 0);
-}
+	sendCount = send(currentSocket, headerOut, HEADER_SIZE, 0);
+	sendCount += send(currentSocket, data, size, 0);
+	return sendCount;
+};
 
 
 int UdsRegWorker::transmit(RsdMsg* msg)
 {
+	int sendCount = 0;
+	createHeader(headerOut, msg->getContent()->size());
 	log(logInfoOut, msg->getContent());
-	return send(currentSocket, msg->getContent()->c_str(), msg->getContent()->size(), 0);
-}
+	sendCount = send(currentSocket, headerOut, HEADER_SIZE, 0);
+	sendCount += send(currentSocket, msg->getContent()->c_str(), msg->getContent()->size(), 0);
+	return sendCount;
+};
 
 
